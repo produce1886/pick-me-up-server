17e50165f9e0c8e09523d947d16cc6b04626c117
amy choi <cgc8016@ewhain.net>
2021-01-10 21:51:02 +0900

    feat(pick-me-up): first commit for project setting

---
+++ src/main/java/com/produce/pickmeup/PickmeupApplication.java
@@ -0,0 +1,13 @@
+package com.produce.pickmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickmeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickmeupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/produce/pickmeup/PickmeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produce.pickmeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickmeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
