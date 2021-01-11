0a98438828f2f16222a4e778e30dcf63e1c24649
amy choi <cgc8016@ewhain.net>
2021-01-12 02:13:51 +0900

    feat(loginController): add error handling logic for no email field

--- src/main/java/com/produce/pickmeup/controller/UserController.java
+++ src/main/java/com/produce/pickmeup/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.produce.pickmeup.controller;
 
 
+import com.produce.pickmeup.common.ErrorCase;
+import com.produce.pickmeup.domain.ErrorMessage;
 import com.produce.pickmeup.domain.user.UserRequestDto;
-import com.produce.pickmeup.domain.user.UserResponseDto;
 import com.produce.pickmeup.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +18,12 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/login")
-	public ResponseEntity<UserResponseDto> loginController(@RequestBody UserRequestDto userRequestDto) {
+	public ResponseEntity<Object> loginController(@RequestBody UserRequestDto userRequestDto) {
+		if (userRequestDto.getEmail() == null) {
+			return new ResponseEntity<>(
+				new ErrorMessage(HttpStatus.BAD_REQUEST, ErrorCase.INVALID_FIELD_ERROR),
+				HttpStatus.BAD_REQUEST);
+		}
 		return new ResponseEntity<>(userService.login(userRequestDto), HttpStatus.OK);
 	}
 }
