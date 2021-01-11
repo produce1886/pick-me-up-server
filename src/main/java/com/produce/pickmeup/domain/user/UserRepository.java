8f3010452d49846645b446f3a38e7da3b4c2807f
amy choi <cgc8016@ewhain.net>
2021-01-11 16:51:22 +0900

    feat(userRepository): add UserRepository using JPA repository

---
+++ src/main/java/com/produce/pickmeup/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.produce.pickmeup.domain.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+}
