17f3777c609dee11bb9b742e59fa7e8cc240bb5b
hoit1302 <jueun000101@gmail.com>
2021-01-14 03:29:13 +0900

    feat(UserService): add updateUserInfo

--- src/main/java/com/produce/pickmeup/service/UserService.java
+++ src/main/java/com/produce/pickmeup/service/UserService.java
@@ -47,4 +47,9 @@ public class UserService {
 	public Optional<User> findById(Long id) {
 		return userRepository.findById(id);
 	}
+
+	@Transactional
+	public void updateUserInfo(User user, UserInfoDto userInfo) {
+		user.updateInfo(userInfo);
+	}
 }
