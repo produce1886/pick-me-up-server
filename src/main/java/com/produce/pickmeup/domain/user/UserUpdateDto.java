3841bd5d553994bb38723c84f8a8df9765451ec3
amy choi <cgc8016@ewhain.net>
2021-01-14 20:46:21 +0900

    feat(UserUpdateDto): make new dto for user update

---
+++ src/main/java/com/produce/pickmeup/domain/user/UserUpdateDto.java
@@ -0,0 +1,23 @@
+package com.produce.pickmeup.domain.user;
+
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserUpdateDto {
+	private final String username;
+	private final String introduce;
+	private final Date birth;
+	private final String university;
+	private final String major;
+	private final String region;
+	private final String interests;
+	private final boolean isBirthPublic;
+	private final boolean isUniversityPublic;
+	private final boolean isRegionPublic;
+	private final boolean isInterestsPublic;
+}
