0489674f7c3d4f46ac65912ecf400a470ec9b55b
amy choi <cgc8016@ewhain.net>
2021-01-23 03:04:09 +0900

    feat(addProjectComment): add controller, service, repository for new project comment

---
+++ src/main/java/com/produce/pickmeup/domain/project/comment/ProjectCommentRepository.java
@@ -0,0 +1,6 @@
+package com.produce.pickmeup.domain.project.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectCommentRepository extends JpaRepository<ProjectComment, Long> {
+}
---
+++ src/main/java/com/produce/pickmeup/service/ProjectCommentService.java
@@ -0,0 +1,42 @@
+package com.produce.pickmeup.service;
+
+import com.produce.pickmeup.common.ErrorCase;
+import com.produce.pickmeup.domain.project.Project;
+import com.produce.pickmeup.domain.project.ProjectRepository;
+import com.produce.pickmeup.domain.project.comment.ProjectComment;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentRepository;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentRequestDto;
+import com.produce.pickmeup.domain.user.User;
+import com.produce.pickmeup.domain.user.UserRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProjectCommentService {
+	private final UserRepository userRepository;
+	private final ProjectCommentRepository projectCommentRepository;
+	private final ProjectRepository projectRepository;
+
+	@Transactional
+	public String addProjectComment(ProjectCommentRequestDto projectCommentRequestDto, Long projectId) {
+		Optional<User> author = userRepository.findByEmail(projectCommentRequestDto.getEmail());
+		Optional<Project> project = projectRepository.findById(projectId);
+		if (!author.isPresent()) {
+			return ErrorCase.NO_SUCH_USER;
+		}
+		if (!project.isPresent()) {
+			return ErrorCase.NO_SUCH_PROJECT;
+		}
+		long result = projectCommentRepository.save(
+			ProjectComment.builder()
+				.author(author.get())
+				.content(projectCommentRequestDto.getContent())
+				.project(project.get())
+				.build())
+			.getId();
+		return String.valueOf(result);
+	}
+}
