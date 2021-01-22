0c9c42bc512141657d251cc60758cf84c4b74dba
amy choi <cgc8016@ewhain.net>
2021-01-23 03:04:09 +0900

    feat(addProjectComment): add controller, service, repository for new project comment

---
+++ src/main/java/com/produce/pickmeup/controller/ProjectCommentController.java
@@ -0,0 +1,34 @@
+package com.produce.pickmeup.controller;
+
+import com.produce.pickmeup.common.ErrorCase;
+import com.produce.pickmeup.domain.ErrorMessage;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentRequestDto;
+import com.produce.pickmeup.service.ProjectCommentService;
+import java.net.URI;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@AllArgsConstructor
+public class ProjectCommentController {
+	private final List<String> REQUEST_ERROR_LIST = ErrorCase.getRequestErrorList();
+	private final ProjectCommentService projectCommentService;
+
+	@PostMapping("/projects/{id}/comment")
+	public ResponseEntity<Object> addProjectComment(@PathVariable Long id,
+		@RequestBody ProjectCommentRequestDto projectCommentRequestDto) {
+		String result = projectCommentService.addProjectComment(projectCommentRequestDto, id);
+		if (REQUEST_ERROR_LIST.contains(result)) {
+			return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST)
+				.body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), result));
+		}
+		return ResponseEntity.created(URI.create("/projects/" + id + "/comments" + result)).build();
+	}
+}
