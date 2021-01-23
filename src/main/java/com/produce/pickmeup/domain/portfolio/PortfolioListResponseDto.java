199460a5779e5fdf99b6bd1c48ef4a078d2119dc
hoit1302 <jueun000101@gmail.com>
2021-01-23 12:35:32 +0900

    fix(PortfolioListResponseDto): make PortfolioListResponseDto field final

--- src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioListResponseDto.java
+++ src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioListResponseDto.java
@@ -3,15 +3,13 @@ package com.produce.pickmeup.domain.portfolio;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class PortfolioListResponseDto {
-    private int totalNum;
-    private List<PortfolioDto> portfolioList;
+    private final int totalNum;
+    private final List<PortfolioDto> portfolioList;
 }
