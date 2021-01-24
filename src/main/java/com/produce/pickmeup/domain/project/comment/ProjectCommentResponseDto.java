322dc8e3b4cfcd2c7481ddfd3dba9a8867d007af
amy choi <cgc8016@ewhain.net>
2021-01-25 01:03:56 +0900

    feat(ProjectCommentResponseDto): add ProjectCommentResponseDto

---
+++ src/main/java/com/produce/pickmeup/domain/project/comment/ProjectCommentResponseDto.java
@@ -0,0 +1,18 @@
+package com.produce.pickmeup.domain.project.comment;
+
+import com.produce.pickmeup.domain.login.LoginResponseDto;
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ProjectCommentResponseDto {
+	private final Timestamp createdDate;
+	private final Timestamp modifiedDate;
+	private final Long id;
+	private final String content;
+	private final LoginResponseDto user;
+}
