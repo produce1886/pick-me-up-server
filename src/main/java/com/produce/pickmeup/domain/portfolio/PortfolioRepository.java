335c16cdf9c7a83bfbf6385ba5d45cb552fed869
hoit1302 <jueun000101@gmail.com>
2021-01-25 13:43:43 +0900

    feat(PortfolioRepository): add PortfolioRepository

---
+++ src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioRepository.java
@@ -0,0 +1,7 @@
+package com.produce.pickmeup.domain.portfolio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
+
