a15c373cd6512e1ce6cd4ed763f8581c8451d985
hoit1302 <jueun000101@gmail.com>
2021-01-26 16:19:12 +0900

    feat(PortfolioCommentRepository): add PortfolioCommentRepository

---
+++ src/main/java/com/produce/pickmeup/domain/portfolio/comment/PortfolioCommentRepository.java
@@ -0,0 +1,6 @@
+package com.produce.pickmeup.domain.portfolio.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioCommentRepository extends JpaRepository<PortfolioComment, Long> {
+}
