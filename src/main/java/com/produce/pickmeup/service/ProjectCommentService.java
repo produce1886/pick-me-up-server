4258ceb81ed00ef9bfc71397032666a753035d6b
hoit1302 <jueun000101@gmail.com>
2021-01-29 13:17:59 +0900

    feat(ProjectCommentService): add update comment process

--- src/main/java/com/produce/pickmeup/service/ProjectCommentService.java
+++ src/main/java/com/produce/pickmeup/service/ProjectCommentService.java
@@ -3,16 +3,16 @@ package com.produce.pickmeup.service;
 import com.produce.pickmeup.common.ErrorCase;
 import com.produce.pickmeup.domain.project.Project;
 import com.produce.pickmeup.domain.project.ProjectRepository;
-import com.produce.pickmeup.domain.project.comment.ProjectComment;
-import com.produce.pickmeup.domain.project.comment.ProjectCommentDetailResponseDto;
-import com.produce.pickmeup.domain.project.comment.ProjectCommentRepository;
-import com.produce.pickmeup.domain.project.comment.ProjectCommentRequestDto;
+import com.produce.pickmeup.domain.project.comment.*;
 import com.produce.pickmeup.domain.user.User;
 import com.produce.pickmeup.domain.user.UserRepository;
-import java.util.Optional;
-import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -63,11 +63,26 @@ public class ProjectCommentService {
 			return ErrorCase.NO_SUCH_COMMENT_ERROR;
 		if (!isLinked(projectComment.get(), projectId))
 			return ErrorCase.BAD_REQUEST_ERROR;
-		return "";
+		return "SUCCESS";
 	}
 
 	@Transactional
 	public void deleteCommentDetail(Long projectCommentId) {
 		projectCommentRepository.deleteById(projectCommentId);
+	}
+
+	@Transactional
+	public String updateProjectComment(Long projectId, Long projectCommentId,
+		ProjectCommentRequestDto projectCommentRequestDto) {
+		Optional<Project> project = projectRepository.findById(projectId);
+		if (!project.isPresent())
+			return ErrorCase.NO_SUCH_PROJECT_ERROR;
+		Optional<ProjectComment> projectComment = projectCommentRepository.findById(projectCommentId);
+		if (!projectComment.isPresent())
+			return ErrorCase.NO_SUCH_COMMENT_ERROR;
+		if (!isLinked(projectComment.get(), projectId))
+			return ErrorCase.BAD_REQUEST_ERROR;
+		projectComment.get().updateContent(projectCommentRequestDto);
+		return "SUCCESS";
 	}
 }
