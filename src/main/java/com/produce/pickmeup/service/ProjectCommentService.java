17b1e40dddfc7af84c7a2fde1efdb170c26811ea
amy choi <cgc8016@ewhain.net>
2021-01-29 18:40:26 +0900

    refactor(updateProjectComment): error handling in controller

--- src/main/java/com/produce/pickmeup/service/ProjectCommentService.java
+++ src/main/java/com/produce/pickmeup/service/ProjectCommentService.java
@@ -1,28 +1,26 @@
 package com.produce.pickmeup.service;
 
-import com.produce.pickmeup.common.ErrorCase;
 import com.produce.pickmeup.domain.project.Project;
 import com.produce.pickmeup.domain.project.ProjectRepository;
-import com.produce.pickmeup.domain.project.comment.*;
+import com.produce.pickmeup.domain.project.comment.ProjectComment;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentDetailResponseDto;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentRepository;
+import com.produce.pickmeup.domain.project.comment.ProjectCommentRequestDto;
 import com.produce.pickmeup.domain.user.User;
-import com.produce.pickmeup.domain.user.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.Optional;
+import javax.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class ProjectCommentService {
-	private final UserRepository userRepository;
 	private final ProjectCommentRepository projectCommentRepository;
 	private final ProjectRepository projectRepository;
 
 	@Transactional
-	public String addProjectComment(User author, Project project, ProjectCommentRequestDto responseDto) {
+	public String addProjectComment(User author, Project project,
+		ProjectCommentRequestDto responseDto) {
 		long result = projectCommentRepository.save(
 			ProjectComment.builder()
 				.author(author)
@@ -46,35 +44,14 @@ public class ProjectCommentService {
 		return projectComment.getProject().getId() == projectId;
 	}
 
-	public String isBadRequest(Long projectId, Long projectCommentId){
-		Optional<Project> project = projectRepository.findById(projectId);
-		if (!project.isPresent())
-			return ErrorCase.NO_SUCH_PROJECT_ERROR;
-		Optional<ProjectComment> projectComment = projectCommentRepository.findById(projectCommentId);
-		if (!projectComment.isPresent())
-			return ErrorCase.NO_SUCH_COMMENT_ERROR;
-		if (!isLinked(projectComment.get(), projectId))
-			return ErrorCase.BAD_REQUEST_ERROR;
-		return "SUCCESS";
-	}
-
 	@Transactional
 	public void deleteCommentDetail(Long projectCommentId) {
 		projectCommentRepository.deleteById(projectCommentId);
 	}
 
 	@Transactional
-	public String updateProjectComment(Long projectId, Long projectCommentId,
+	public void updateProjectComment(ProjectComment projectComment,
 		ProjectCommentRequestDto projectCommentRequestDto) {
-		Optional<Project> project = projectRepository.findById(projectId);
-		if (!project.isPresent())
-			return ErrorCase.NO_SUCH_PROJECT_ERROR;
-		Optional<ProjectComment> projectComment = projectCommentRepository.findById(projectCommentId);
-		if (!projectComment.isPresent())
-			return ErrorCase.NO_SUCH_COMMENT_ERROR;
-		if (!isLinked(projectComment.get(), projectId))
-			return ErrorCase.BAD_REQUEST_ERROR;
-		projectComment.get().updateContent(projectCommentRequestDto);
-		return "SUCCESS";
+		projectComment.updateContent(projectCommentRequestDto);
 	}
 }
