76382e177ab854f1bdbaae0456ba59ae8c476744
hoit1302 <jueun000101@gmail.com>
2021-02-01 03:52:22 +0900

    feat(PortfolioCommentRequestDto): add dto to create comment

---
+++ src/main/java/com/produce/pickmeup/domain/portfolio/comment/PortfolioCommentRequestDto.java
@@ -0,0 +1,13 @@
+package com.produce.pickmeup.domain.portfolio.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PortfolioCommentRequestDto {
+	private final String authorEmail;
+	private final String content;
+}
