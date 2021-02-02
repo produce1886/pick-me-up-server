a6c5148ff8011e8844c4bed21fe971e5d2eef316
amy choi <cgc8016@ewhain.net>
2021-02-02 23:14:39 +0900

    feat(Portfolio): fix images String List to PortfolioImageDto List

--- src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.produce.pickmeup.domain.portfolio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioImageRepository extends JpaRepository<PortfolioImage, Long> {
-}
