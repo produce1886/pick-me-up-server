2607a045eee7781ce2803f59392ae1a9ffaa7f50
amy choi <cgc8016@ewhain.net>
2021-02-05 00:28:04 +0900

    feat(PortfolioRepository): find all portfolios using by specification, pageable

--- src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioRepository.java
+++ src/main/java/com/produce/pickmeup/domain/portfolio/PortfolioRepository.java
@@ -1,7 +1,12 @@
 package com.produce.pickmeup.domain.portfolio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long>, JpaSpecificationExecutor<Portfolio> {
+	Page<Portfolio> findAll(Specification<Portfolio> specification, Pageable pageable);
 }
 
