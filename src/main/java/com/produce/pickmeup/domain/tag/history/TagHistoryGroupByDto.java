4779dadc7bc3280cf2ee6790f9eb8f386247814d
amy choi <cgc8016@ewhain.net>
2021-02-16 01:45:48 +0900

    feat(HotTagDto, TagHistoryGroupByDto)

---
+++ src/main/java/com/produce/pickmeup/domain/tag/history/TagHistoryGroupByDto.java
@@ -0,0 +1,14 @@
+package com.produce.pickmeup.domain.tag.history;
+
+import com.produce.pickmeup.domain.tag.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class TagHistoryGroupByDto {
+	private final Tag tag;
+	private final Long score;
+}
