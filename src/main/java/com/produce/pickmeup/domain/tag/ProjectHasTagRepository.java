9c70330ce2168fd2cd674431d0beede5b7ed762c
hoit1302 <jueun000101@gmail.com>
2021-02-18 01:20:48 +0900

    feat(HasTagRepository): add determine post exist function

--- src/main/java/com/produce/pickmeup/domain/tag/ProjectHasTagRepository.java
+++ src/main/java/com/produce/pickmeup/domain/tag/ProjectHasTagRepository.java
@@ -1,11 +1,12 @@
 package com.produce.pickmeup.domain.tag;
 
 import com.produce.pickmeup.domain.project.Project;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProjectHasTagRepository extends JpaRepository<ProjectHasTag, Long> {
 	List<ProjectHasTag> findByProject(Project project);
-	Optional<ProjectHasTag> deleteByProjectAndProjectTag(Project project, Tag tag);
+	void deleteByProjectAndProjectTag(Project project, Tag tag);
+	boolean existsByProjectTag(Tag tag);
 }
