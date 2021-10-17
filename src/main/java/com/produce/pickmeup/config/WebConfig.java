c4f005c478c4fd709cb9d7bc50ccb14bdbbc6370
amy choi <cgc8016@ewhain.net>
2021-10-17 16:32:56 +0900

    style: move directory

--- src/main/java/com/produce/pickmeup/config/WebConfig.java
+++ src/main/java/com/produce/pickmeup/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.produce.pickmeup.controller;
+package com.produce.pickmeup.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
