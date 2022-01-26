54085fe26d29b6c18330509959f355590ab0a368
amy choi <cgc8016@ewhain.net>
2022-01-27 00:04:25 +0900

    style(): code formatting

--- src/main/java/com/produce/pickmeup/service/S3Uploader.java
+++ src/main/java/com/produce/pickmeup/service/S3Uploader.java
@@ -19,64 +19,66 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @PropertySource("classpath:application-aws.yml")
 public class S3Uploader {
-	private final static List<String> IMAGE_EXTENSIONS = Arrays
-		.asList(".jpg", ".jpeg", ".gif", ".png", ".img", ".tiff", ".heif");
-	private final static String TEMP_FILE_PATH = "pick-me-up-server/src/main/resources/";
-	private final AmazonS3Client amazonS3Client;
-	@Value("${cloud.aws.s3.bucket}")
-	public String bucket;
 
-	public File convert(MultipartFile file) {
-		File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
-		try {
-			if (convertFile.createNewFile()) {
-				try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-					fos.write(file.getBytes());
-				}
-				return convertFile;
-			}
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		return null;
-	}
+    private static final List<String> IMAGE_EXTENSIONS = Arrays.asList(".jpg", ".jpeg", ".gif", ".png", ".img", ".tiff", ".heif");
+    private static final String TEMP_FILE_PATH = "pick-me-up-server/src/main/resources/";
 
-	public String upload(File uploadFile, String dirName, String id) {
-		String fileName = dirName + "/" + id;
-		String uploadImageUrl = putS3(uploadFile, fileName);
-		deleteLocalFile(uploadFile);
-		return uploadImageUrl;
-	}
+    private final AmazonS3Client amazonS3Client;
 
-	public boolean isValidExtension(File uploadFile) {
-		String fileName = uploadFile.getName();
-		String extension = "." + fileName.substring(fileName.toLowerCase().lastIndexOf(".") + 1);
-		if (!IMAGE_EXTENSIONS.contains(extension)) {
-			deleteLocalFile(uploadFile);
-			return false;
-		}
-		return true;
-	}
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
 
-	public void delete(String dirName, String id) {
-		deleteFromS3(dirName + "/" + id);
-	}
+    public File convert(MultipartFile file) {
+        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
+        try {
+            if (convertFile.createNewFile()) {
+                try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                    fos.write(file.getBytes());
+                }
+                return convertFile;
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
 
-	private void deleteFromS3(String key) {
-		amazonS3Client.deleteObject(bucket, key);
-	}
+    public String upload(File uploadFile, String dirName, String id) {
+        String fileName = dirName + "/" + id;
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        deleteLocalFile(uploadFile);
+        return uploadImageUrl;
+    }
 
-	private String putS3(File uploadFile, String fileName) {
-		PutObjectResult result = amazonS3Client.putObject(
-			new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
-				CannedAccessControlList.PublicRead));
-		return String.format("%s?versionId=%s", amazonS3Client.getUrl(bucket, fileName),
-			result.getVersionId());
-	}
+    public boolean isValidExtension(File uploadFile) {
+        String fileName = uploadFile.getName();
+        String extension = "." + fileName.substring(fileName.toLowerCase().lastIndexOf(".") + 1);
+        if (!IMAGE_EXTENSIONS.contains(extension)) {
+            deleteLocalFile(uploadFile);
+            return false;
+        }
+        return true;
+    }
 
-	private void deleteLocalFile(File uploadFile) {
-		if (!uploadFile.delete()) {
-			System.err.println(ErrorCase.FAIL_FILE_DELETE_ERROR);
-		}
-	}
+    public void delete(String dirName, String id) {
+        deleteFromS3(dirName + "/" + id);
+    }
+
+    private void deleteFromS3(String key) {
+        amazonS3Client.deleteObject(bucket, key);
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        PutObjectResult result = amazonS3Client.putObject(
+            new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
+                CannedAccessControlList.PublicRead));
+        return String.format("%s?versionId=%s", amazonS3Client.getUrl(bucket, fileName),
+            result.getVersionId());
+    }
+
+    private void deleteLocalFile(File uploadFile) {
+        if (!uploadFile.delete()) {
+            System.err.println(ErrorCase.FAIL_FILE_DELETE_ERROR);
+        }
+    }
 }
