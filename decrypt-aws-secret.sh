222809987eed4140c7dea4d59da793c061fca8e8
amy choi <cgc8016@ewhain.net>
2022-01-26 23:48:26 +0900

    fix(decrypt-aws-secret.sh): fix directory name typo

--- decrypt-aws-secret.sh
+++ decrypt-aws-secret.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./src/main/resource/application-aws.yml ./src/main/resource/application-aws.yml.gpg
+--output ./src/main/resources/application-aws.yml ./src/main/resources/application-aws.yml.gpg
