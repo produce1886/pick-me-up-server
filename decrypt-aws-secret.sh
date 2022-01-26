f9525c9ee6ad7eee1e8bb44e83b5cfc393de729e
amy choi <cgc8016@ewhain.net>
2022-01-26 23:58:40 +0900

    chore(ci.yml): add application.yml encrypt file, decrypt script

--- decrypt-aws-secret.sh
+++ decrypt-aws-secret.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output ./src/main/resources/application-aws.yml ./src/main/resources/application-aws.yml.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output ./src/main/resources/application.yml ./src/main/resources/application.yml.gpg
